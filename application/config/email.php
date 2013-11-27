46df13998bf05179c1a3243ee9f41915f5fa9aa8
root <kutaliatato@gmail.com>
2013-11-28 00:26:09 +0400

    test: send email

---
+++ application/config/email.php
@@ -0,0 +1,8 @@
+<?php
+    $config['protocol'] = 'smtp';
+    $config['smtp_host'] = 'ssl://smtp.gmail.com';
+    $config['smtp_port'] = '465';
+    $config['smtp_user'] = 'tato@innotec.ge';
+    $config['smtp_pass'] = '<3tato<3';
+    $config['charset'] = 'utf-8';
+    $config['newline'] = "\r\n";
