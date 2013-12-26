75dd9ff86bc2fa956ca40f12a962edb8a6c09740
Merab Tato Kutalia <tatocaster@users.noreply.github.com>
2013-12-26 12:57:46 -0800

    Update email.php

--- application/config/email.php
+++ application/config/email.php
@@ -2,7 +2,7 @@
     $config['protocol'] = 'smtp';
     $config['smtp_host'] = 'ssl://smtp.gmail.com';
     $config['smtp_port'] = '465';
-    $config['smtp_user'] = 'tato@innotec.ge';
-    $config['smtp_pass'] = '<3tato<3';
+    $config['smtp_user'] = '****';
+    $config['smtp_pass'] = '****';
     $config['charset'] = 'utf-8';
     $config['newline'] = "\r\n";
